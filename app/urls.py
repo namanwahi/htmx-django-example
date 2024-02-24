59b7bd680778681f875bf1d3505992263705fc09
Nams <namanwahi@yahoo.co.uk>
2024-02-24 12:04:14 +0000

    validate user email

--- app/urls.py
+++ app/urls.py
@@ -10,3 +10,9 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("login/", views.LoginView.as_view(), name="login"),
 ]
+
+
+htmx_urlpatterns = [path("check_email/", views.check_email, name="check_email")]
+
+
+urlpatterns += htmx_urlpatterns
